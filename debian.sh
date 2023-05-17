ace331476d9899d3b6543359a208facce6945551
demian-vdb <100292955+demian-vdb@users.noreply.github.com>
2023-05-17 23:26:49 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -10,7 +10,7 @@ docker run -d --name=grafana --restart=unless-stopped -p 3000:3000 grafana/grafa
 sh -c 'echo "" >> /etc/network/interfaces'
 sh -c 'echo "auto enp0s8" >> /etc/network/interfaces'
 sh -c 'echo "iface enp0s8 inet static" >> /etc/network/interfaces'
-sh -c 'echo "    address 192.168.0.20" >> /etc/network/interfaces'
+sh -c 'echo "    address 192.168.10.20" >> /etc/network/interfaces'
 sh -c 'echo "    netmask 255.255.255.0" >> /etc/network/interfaces'
 
 shutdown now -r
